f638913e297ea007bf0733c6fbb7c1bd720ae99d
raphaelrighetti <raphaelrighettiramos@hotmail.com>
2023-10-06 18:19:32 -0300

    adicionando pasta de criacao do banco de dados

---
+++ mongo/scripts/init.js
@@ -0,0 +1,7 @@
+db.createUser({
+  user: process.env.MONGO_INITDB_ROOT_USERNAME,
+  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  roles: [{ role: 'readWrite', db: process.env.MONGO_INITDB_DATABASE }],
+});
+
+db.createCollection('mensagens', { capped: false });
